631dba1c07cbb0c61826a4dea4c21253cab74ee9
h4dicicek <h4dicicek@gmail.com>
2022-06-24 16:29:26 +0300

    Comment line and km variable added on file.

--- carfuelcalculator.py
+++ carfuelcalculator.py
@@ -7,3 +7,7 @@ fuel_money_liter = 27.26
 # You must get how much fuel(liter) is burned per km from user.
 
 fuel_liter_km = float(input("Please enter how much fuel(liter) is burned per km your car: "))
+
+#And you should know how many kilometers to go from the user.
+
+km = int(input("Please enter how many kilometers to go with your car: "))
