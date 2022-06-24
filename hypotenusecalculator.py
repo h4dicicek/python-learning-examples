e79990299aef3631af6a904b5eef9b18eb28593d
h4dicicek <h4dicicek@gmail.com>
2022-06-24 16:38:56 +0300

    x and y variables added on file.

--- hypotenusecalculator.py
+++ hypotenusecalculator.py
@@ -1,1 +1,4 @@
 #In this file, we will take the 2 edges of a perpendicular triangle and calculate its hypotenuse.
+
+x = int(input("Please enter first edge: "))
+y = int(input("Please enter second edge: ")) 
