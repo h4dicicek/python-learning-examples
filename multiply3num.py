7bfba9421d553db2f0b64caf03a53a26285c5ef8
h4dicicek <h4dicicek@gmail.com>
2022-06-24 16:10:30 +0300

    Code is written and completed.

--- multiply3num.py
+++ multiply3num.py
@@ -1,3 +1,9 @@
 #We will multiply 3 number in this file.
 
 #You must take 3 number from user.
+
+x = int(input("Please enter a number: "))
+y = int(input("Please enter a number: "))
+z = int(input("Please enter a number: "))
+
+print("Result of process: {}".format(x*y*z))
