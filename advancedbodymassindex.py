ff8e79c1fb3796cb08546b67b140c7fb60fca7f2
h4dicicek <h4dicicek@gmail.com>
2022-06-25 14:35:23 +0300

    BMI table added on file for conditions.

--- advancedbodymassindex.py
+++ advancedbodymassindex.py
@@ -1,5 +1,16 @@
 #We will calculate the body mass index(bmi) in this file.
 
+"""
+Severe Thinness	< 16
+Moderate Thinness	16 - 17
+Mild Thinness	17 - 18.5
+Normal	18.5 - 25
+Overweight	25 - 30
+Obese Class I	30 - 35
+Obese Class II	35 - 40
+Obese Class III	> 40 
+"""
+
 #First of all, you must take body weight and height from the user.
 
 body_weight = float(input("Please enter your body weight(kg): "))
