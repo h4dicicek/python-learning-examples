d87a9d64ec6e5bae811eaca2f741f13fd8c8244f
h4dicicek <h4dicicek@gmail.com>
2022-06-25 14:28:15 +0300

    body_weight and body_height variables added on file.

--- advancedbodymassindex.py
+++ advancedbodymassindex.py
@@ -1,3 +1,7 @@
 #We will calculate the body mass index(bmi) in this file.
 
 #First of all, you must take body weight and height from the user.
+
+body_weight = float(input("Please enter your body weight(kg): "))
+
+body_height = float(input("Please enter your body height(m): ")
