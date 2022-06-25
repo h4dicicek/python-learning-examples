a19f82a33aafc2487ce974e7e0898b1f34ebcbc0
h4dicicek <h4dicicek@gmail.com>
2022-06-25 14:48:34 +0300

    Conditions added on file. Code is completed.

--- advancedbodymassindex.py
+++ advancedbodymassindex.py
@@ -18,3 +18,23 @@ body_weight = float(input("Please enter your body weight(kg): "))
 body_height = float(input("Please enter your body height(m): "))
 
 body_mass_index = body_weight / (body_height ** 2)
+
+print("Your BMI {}".format(body_mass_index))
+
+if (body_mass_index < 16):
+    print("You're Severe Thinness.") 
+
+elif (16 < body_mass_index < 17):
+    print("You're Moderate Thinnes")
+
+elif (17 < body_mass_index < 18.5):
+    print("You're Mild Thinness.")
+
+elif (18.5 < body_mass_index < 25):
+    print("You're Normal weight.")
+
+elif (25 < body_mass_index < 30):
+    print("You're owerweight.")
+
+elif (30 < body_mass_index):
+    print("You're obese.")
