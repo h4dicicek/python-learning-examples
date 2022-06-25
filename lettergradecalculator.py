5004dd0acc5354aae7f6e47ee93bbc599023bea6
h4dicicek <h4dicicek@gmail.com>
2022-06-25 15:02:33 +0300

    Comment line and table of letter grade added on file.

---
+++ lettergradecalculator.py
@@ -0,0 +1,28 @@
+# We'are gonna calculate letter grade for university students. 
+
+"""
+Midterm1 will impact 30% of the total grade.
+
+Midterm2 will impact 30% of the total grade.
+
+Final will impact 40% of the total grade.
+
+
+Toplam Not >=  90 -----> AA
+
+Toplam Not >=  85 -----> BA
+
+Toplam Not >=  80 -----> BB
+
+Toplam Not >=  75 -----> CB
+
+Toplam Not >=  70 -----> CC
+
+Toplam Not >=  65 -----> DC
+
+Toplam Not >=  60 -----> DD
+
+Toplam Not >=  55 -----> FD
+
+Toplam Not <  55 -----> FF
+"""
