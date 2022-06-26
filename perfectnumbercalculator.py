de65df373079546ec360837183bd2cf1f62457d2
h4dicicek <h4dicicek@gmail.com>
2022-06-26 15:16:28 +0300

    number variable added on file.

--- perfectnumbercalculator.py
+++ perfectnumbercalculator.py
@@ -1,3 +1,5 @@
 # In this file we calculate perfect number.
 # In number theory, a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
 # For instance, 6 has divisors 1, 2 and 3 (excluding itself), and 1 + 2 + 3 = 6, so 6 is a perfect number.
+
+number = int(input("Please enter a number: "))
