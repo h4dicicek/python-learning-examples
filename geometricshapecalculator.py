97e7f3ae8539070a8c12cfc3bedc1963143a3102
h4dicicek <h4dicicek@gmail.com>
2022-06-26 14:15:55 +0300

    Triangle types added on file.

--- geometricshapecalculator.py
+++ geometricshapecalculator.py
@@ -17,10 +17,16 @@ ______________________
 selected_shape =  int(input("Please enter a number: "))
 
 if ( selected_shape == 1):
-    print("Triangle selected by user.")
+    print("Triangle selected by user. Please enter the triangle lengths in the order from small to large.")
     x = int(input("Please enter first edge: "))
     y = int(input("Please enter second edge: "))
     z = int(input("Please enter third edge: "))
     
     #We have to use abs to obtain its absolute value.
     print("Your triangle edge lenghts: {} , {} , {}".format(abs(x),abs(y),abs(z)))
+    if((x == y and y != z) or (y == z and z != x) or (x == z and z != y)):
+        print("Your triangle is Isosceles Triangle.")
+    elif(x == y and y == z):
+        print("Your triangle is Equilateral Triangle.")
+    else:
+        print("Your triangle is normal triangle :).")
