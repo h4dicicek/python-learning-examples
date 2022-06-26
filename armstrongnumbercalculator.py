d1df9d2af0ac09c50965c473470a6241493850c2
h4dicicek <h4dicicek@gmail.com>
2022-06-26 17:12:51 +0300

    number and number_of_digits variables added on file.

--- armstrongnumbercalculator.py
+++ armstrongnumbercalculator.py
@@ -1,3 +1,9 @@
 # In this file we calculate Armstrong Number.
 # An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. 
 # For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
+
+number = input("Please enter a number: ")
+
+number_of_digits = len(number)
+
+number = int(number)
