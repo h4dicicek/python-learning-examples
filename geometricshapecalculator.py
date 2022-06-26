2810a04cb59a751ab30b6e460fad8967f05ab475
h4dicicek <h4dicicek@gmail.com>
2022-06-26 13:45:46 +0300

    A string added on file for to know which shape choosen from user.

--- geometricshapecalculator.py
+++ geometricshapecalculator.py
@@ -1,1 +1,12 @@
 # In this file, we will get a shape type and lengths from user. And calculate is what type of them shape. 
+
+# First, we have to know which shape calculate.
+
+print("""
+______________________
+                      
+PRESS 1 FOR TRIANGLE  
+                      
+PRESS 2 FOR RECTANGLE 
+______________________
+""")
