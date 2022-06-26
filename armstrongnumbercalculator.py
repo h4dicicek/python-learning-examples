5f5d5f4c0eb5c2435b967cfd4ff02a5a64d7c6ce
h4dicicek <h4dicicek@gmail.com>
2022-06-26 17:04:06 +0300

    Comment lines added on file for description of Armstrong number.

---
+++ armstrongnumbercalculator.py
@@ -0,0 +1,3 @@
+# In this file we calculate Armstrong Number.
+# An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. 
+# For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
