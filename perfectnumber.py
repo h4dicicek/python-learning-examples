d51eb1e65eccf90a2c98af75dd430f37b5a14833
h4dicicek <h4dicicek@gmail.com>
2022-06-26 22:41:31 +0300

    Perfectnumber function is completed.

---
+++ perfectnumber.py
@@ -0,0 +1,14 @@
+# In this file we calculate perfect number. Between 1 and 1000
+
+# We have to create a new function with use def.
+
+def perfectnumber(number):
+
+    total = 0 
+
+    for i in range(1,number):
+
+        if (number % i == 0):
+            total += i
+
+    return total == number
