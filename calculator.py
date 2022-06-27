e82ca3a8c30e3e11418951b3ac9e0fe4e6c0a3da
h4dicicek <h4dicicek@gmail.com>
2022-06-27 15:05:34 +0300

    Operation table added on file.

--- calculator.py
+++ calculator.py
@@ -1,5 +1,47 @@
 #In this file we create calculator with math module.
 
-#we have to import math module
+#we have to import math module to use methods in module.
 
 import math
+
+# We have to create table of operations.
+
+print("""
+___________________________________________
+
+Operations:
+
+Press 1 for add
+
+Press 2 for subtract 
+
+Press 3 for divide 
+
+Press 4 for multiply
+
+Press 5 for absolute value
+
+Press 6 for factorial
+
+Press 7 for gcd
+
+Press 8 for logarithm
+
+Press 9 for x^y
+
+Press 10 for squareroot
+
+Press 11 for to change radian to degrees
+
+Press 12 for to change degrees to radian
+
+Press 13 for calculate sin value in radian
+
+Press 14 for calculate cos value in radian
+
+Press 15 for calculate tan value in radian
+
+PRESS 16 FOR QUIT
+___________________________________________
+
+""")
