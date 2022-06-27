b5a6e413519f97188a24215f603a1d02529fa937
h4dicicek <h4dicicek@gmail.com>
2022-06-27 14:10:33 +0300

    Function is defined.

--- lowestcommonmultiple.py
+++ lowestcommonmultiple.py
@@ -1,1 +1,19 @@
 # In this file we calculate lowest common multiple between two number.
+
+# Now we have to define a function.
+
+def lowestcommonmultiple(x,y):
+
+    if (x > y):
+        bigger = x
+    else:
+        bigger = y
+    
+    while True:
+        if((bigger % x == 0 ) and (bigger % y == 0 )):
+
+            lcm = bigger
+            break
+    
+        bigger += 1
+    return lcm
