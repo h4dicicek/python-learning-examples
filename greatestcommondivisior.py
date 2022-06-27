ad296c59af27feac9ba580906a90f21e72bdbe91
h4dicicek <h4dicicek@gmail.com>
2022-06-27 13:49:07 +0300

    Function is defined.

--- greatestcommondivisior.py
+++ greatestcommondivisior.py
@@ -1,1 +1,17 @@
 # This file for calculate greatest common divisior (gcd) between two number.
+
+# First of all we have to define function with def function.
+
+def greatestcommondivisior(y, x):
+    
+    if (x > y) :
+        small_number = y
+    
+    else:
+        small_number = x
+
+    for i in range(1,small_number+1):
+        if ((x % i == 0 ) and (y % i == 0)):
+            gcd = i
+
+    return gcd
