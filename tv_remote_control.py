c7cbf86985c627b7719aa70a0e6cc5167e7ed0ac
h4dicicek <h4dicicek@gmail.com>
2022-06-29 01:00:21 +0300

    tvRemoteControl class is created and __init__ function is defined.

--- tv_remote_control.py
+++ tv_remote_control.py
@@ -1,1 +1,12 @@
 # We'll try to make a tv remote control in this file.
+
+# We have to create class.
+
+class tvRemoteControl():
+
+    # define __init__  function
+    def __init__(self,tv_status = "off",tv_volume = 0,tv_channel = "Dmax",tv_channel_list = "Dmax"):
+        self.tv_status = tv_status
+        self.tv_volume = tv_volume
+        self.tv_channel = tv_channel
+        self.tv_channel_list = tv_channel_list
