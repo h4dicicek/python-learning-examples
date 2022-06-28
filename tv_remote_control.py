1ff67b7932a229a1d3313315edde147052340b40
h4dicicek <h4dicicek@gmail.com>
2022-06-29 01:29:08 +0300

    addChannel function is defined.

--- tv_remote_control.py
+++ tv_remote_control.py
@@ -8,7 +8,7 @@ import time
 class tvRemoteControl():
 
     # define __init__  function
-    def __init__(self,tv_status = "off",tv_volume = 0,tv_channel = "Dmax",tv_channel_list = "Dmax"):
+    def __init__(self,tv_status = "off",tv_volume = 0,tv_channel = "Dmax",tv_channel_list = ["Dmax"]):
         self.tv_status = tv_status
         self.tv_volume = tv_volume
         self.tv_channel = tv_channel
@@ -35,9 +35,14 @@ class tvRemoteControl():
         time.sleep(0.4)
         print("Volume: ",self.tv_volume)
     
-    # tvOff funtion is for turn off tv
+    # tvOff function is for turn off tv
     def tvOff(self,tv_off):
         time.sleep(0.7)
         print("TV is off.")
         tv_off = "off"
         self.tv_status = tv_off
+    
+    # addChannel function is for add new channel on Channel list
+    def addChannel(self,newChannel):
+        newChannel = str(input("Please enter which channel do you want add on Channel List: "))
+        self.tv_channel_list.append(newChannel)
