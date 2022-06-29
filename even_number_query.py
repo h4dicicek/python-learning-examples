346a36bf9c524d724546dbe929ba6dc6c310d4da
h4dicicek <h4dicicek@gmail.com>
2022-06-29 22:07:54 +0300

    Function is updated.

--- even_number_query.py
+++ even_number_query.py
@@ -1,6 +1,13 @@
 def evenNumber(s):
-    if (s % 2 != 0):
-        raise ValueError("Please enter an even number!")
+    if (s % 2 == 0):
+        return s
     else:
-        return s 
+        raise ValueError
 
+list_1 = [12,32,34,62,8,35,3,654,23,45,2]
+
+for number in list_1:
+    try:
+        print(evenNumber(number))
+    except:
+        pass
