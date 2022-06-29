2e26a9f7bdf0b7b12b615603f7e1215e07fc60be
h4dicicek <h4dicicek@gmail.com>
2022-06-29 12:43:28 +0300

    Bug fixed. File is working.

--- tv_remote_control.py
+++ tv_remote_control.py
@@ -14,46 +14,46 @@ class tvRemoteControl():
         self.tv_channel = tv_channel
         self.tv_channel_list = tv_channel_list
     
-    def __str__(self):
-        return "TV Status: {} \nTV Volume: {} \nChannel: {} \nChannel List {}".format(self.tv_status,self.tv_volume,self.tv_channel,self.tv_channel_list)
+    def tvInformations(self):
+        print("TV Status: {} \nTV Volume: {} \nChannel: {} \nChannel List {}".format(self.tv_status,self.tv_volume,self.tv_channel,self.tv_channel_list))
     
     # tvOn function is for turn on tv
-    def tvOn(self, tv_on):
+    def tvOn(self):
         if (self.tv_status == "on"):
             print("TV still on.")
         else:
             time.sleep(0.7)
-            tv_on = "on"
-            self.tv_status = tv_on
+            self.tv_status = "on"
             print("TV is opening.")
 
     # changeVolume function is for changing tv volume
     def changeVolume(self):
         while True:
-            changeVolumQuantity = input("For lower '<': \nFor higher '>': ")
+            changeVolumQuantity = input("For lower '<': \nFor higher '>': \nExit 'another button': ")
 
             if (changeVolumQuantity == "<"):
                 if (self.tv_volume != 0):
                     self.tv_volume -= 1
-            
+                    print("TV Volume: {}".format(self.tv_volume))
             elif(changeVolumQuantity == ">"):
                 if (self.tv_volume != 100):
                     self.tv_volume += 1
-            print("TV Volume: {}".format(self.tv_volume))
-        
-    
+                    print("TV Volume: {}".format(self.tv_volume))
+            else:
+                print("TV Volume changed.")
+                break
+
     # tvOff function is for turn off tv
-    def tvOff(self,tv_off):
+    def tvOff(self):
         if (self.tv_status == "off"):
             print("TV still off.")
         else:
             time.sleep(0.7)
-            tv_off = "off"
-            self.tv_status = tv_off
-            print("TV is off.")
-    
+            print("TV is turning off.")
+            self.tv_status = "off"
+
     # addChannel function is for add new channel on Channel list
-    def addChannel(self,newChannel):
+    def addChannel(self):
         newChannel = input("Please enter which channel do you want add on Channel List: ")
         self.tv_channel_list.append(newChannel)
    
@@ -86,36 +86,36 @@ Press 6 for learn the number of channels.
 
 Press 7 for TV Informations.
 
-Press 'q' for exit.
+Press q for exit.
 ''')
 
 while True:
     operation = input("Please enter operation: ")
 
-    if (operation == 'q'):
+    if (operation == "q"):
         print("Exit from operations.")
         break
-    
-    elif (operation == '1'):
+
+    elif (operation == "1"):
         tv_remote_control.tvOn()
     
-    elif (operation == '2'):
+    elif (operation == "2"):
         tv_remote_control.tvOff()
 
-    elif (operation == '3'):
+    elif (operation == "3"):
         tv_remote_control.changeVolume()
 
-    elif (operation == '4'):
+    elif (operation == "4"):
         tv_remote_control.addChannel()
     
-    elif (operation == '5'):
+    elif (operation == "5"):
         tv_remote_control.switchChannel()   
     
-    elif (operation == '6'):
+    elif (operation == "6"):
         print("Number of Channels:",len(tv_remote_control))
            
-    elif (operation == '7'):
-        tv_remote_control.__str__
+    elif (operation == "7"):
+        tv_remote_control.tvInformations()
     
     else:
         print("Operation is not found!")
