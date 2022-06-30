6f51afef5b6db37825262be4266863ca0e5dcce1
h4dicicek <h4dicicek@gmail.com>
2022-06-30 15:53:01 +0300

    Function is tested. Code is completed.

--- even_number_query.py
+++ even_number_query.py
@@ -11,3 +11,10 @@ def evenNumber(s):
 # create a list for testing function.
 
 list_1 = [12,32,34,62,8,35,3,654,23,45,2]
+
+for number in list_1:
+    
+    try:
+        print(evenNumber(number))
+    except:
+        pass
