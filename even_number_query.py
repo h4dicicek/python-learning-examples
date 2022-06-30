ed1a345261cbe796680d9b77f99b058f6cd3c5e5
h4dicicek <h4dicicek@gmail.com>
2022-06-30 15:49:10 +0300

    Function is defined.

--- even_number_query.py
+++ even_number_query.py
@@ -1,1 +1,9 @@
 # In this file we'll question whether a number is a even number in this file.
+
+# First we define a function.
+
+def evenNumber(s):
+    if (s % 2 == 0):
+        return s
+    else:
+        raise ValueError
