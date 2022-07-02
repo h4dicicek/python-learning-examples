6b2664faf31be9b07b284a418bedacf1f844e766
h4dicicek <h4dicicek@gmail.com>
2022-07-02 18:59:24 +0300

    Bug fixed. Functions are defined.

--- conditions/geometric_shape_calculator.py
+++ conditions/geometric_shape_calculator.py
@@ -1,66 +1,36 @@
-# In this file, we will get a shape type and lengths from user. And calculate is what type of them shape. 
+import time
 
-# First, we have to know which shape calculate.
-
-from operator import indexOf
-from random import seed
-from re import A
-
-
-print("""
-______________________
-                      
-PRESS 1 FOR TRIANGLE  
-                      
-PRESS 2 FOR RECTANGLE 
-______________________
-""")
-
-selected_shape =  int(input("Please enter a number: "))
-
-if ( selected_shape == 1):
-
-    print("Triangle selected by user. Please enter the triangle lengths in the order from small to large.")
-    x = int(input("Please enter first edge: "))
-    y = int(input("Please enter second edge: "))
-    z = int(input("Please enter third edge: "))
-    
-    #We have to use abs to obtain its absolute value.
-    print("Your triangle edge lenghts: {} , {} , {}".format(abs(x),abs(y),abs(z)))
-    
-    if((x == y and y != z) or (y == z and z != x) or (x == z and z != y)):
-        print("Your shape is Isosceles Triangle.")
-    
-    elif(x == y and y == z):
-        print("Your shape is Equilateral Triangle.")
-    
-    elif( x + y < z):
-        print("Your shape is not triangle. Because first edge + second edge have to bigger than third edge.")
-
+def triangle_calculator(line_1 = int ,line_2 = int,line_3 = int):
+    line_1 = abs(line_1)
+    line_2 = abs(line_2)
+    line_3 = abs(line_3)
+    print("Your triangle is calculating..")
+    time.sleep(0.5)
+    if (line_1 == line_2 == line_3):
+        return "Your triangle is Equilateral Triangle."
+    elif (line_1 + line_2 < line_3):
+        return "The shape is not a triangle."
+    elif ((line_1 == line_2 != line_3) or (line_2 == line_3 != line_1) or (line_1 == line_3 != line_2)):
+        return "Your triangle is Isosceles Triangle."
     else:
-        print("Your shape is normal triangle :).")
+        return "Your triangle is Normal Triangle :)."
 
-elif( selected_shape == 2):
-
-    print("Rectangle selected by user.")
-
-    a = int(input("Please enter first edge: "))
-    b = int(input("Please enter second edge: "))
-    c = int(input("Please enter third edge: "))
-    d = int(input("Please enter fourth edge: "))
-
-    #We have to use abs to obtain its absolute value.
-    print("Your rectangle edge lenghts: {} , {} , {} , {}".format(abs(a),abs(b),abs(c),abs(d)))
-
-    if(a == b and b == c and c == d ):
-        print("Your shape is Square.")
-    elif((a == b and c == d and b != c) or (b == c and a == d and b != a) or (b == d and c == a and b != c)):
-        print("Your shape rectangle :).")
+def rectangle_calculator(line_1 = int ,line_2 = int,line_3 = int,line_4 = int):
+    line_1 = abs(line_1)
+    line_2 = abs(line_2)
+    line_3 = abs(line_3)
+    line_4 = abs(line_4)
+    print("Your rectangle is calculating..")
+    time.sleep(0.5)
+    if (line_1 == line_2 == line_3 == line_4):
+        return "Your rectangle is Square."
+    elif(line_1 == line_3 != line_2 == line_4):
+        return "Your shape is Rectangle."
     else:
-        print("Your shape is normal rectangle :|.")
+        return "Your shape is normal rectangle."
 
-else:
-    print("Shape type not found!")
+print(rectangle_calculator(3,3,3,3))
+    
 
 ### Unfortunately we can't calculate many geometrical shape. Because to calculate that, we have to learn their angle. 
 ### We can do this future projects.
