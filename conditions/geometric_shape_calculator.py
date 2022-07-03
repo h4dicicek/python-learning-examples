abd2d78cf2c8260fd7c8e7b946ba937fdc6e73e9
h4dicicek <h4dicicek@gmail.com>
2022-07-03 22:18:37 +0300

    Bug fixed.

--- conditions/geometric_shape_calculator.py
+++ conditions/geometric_shape_calculator.py
@@ -1,36 +1,65 @@
 import time
 
-def triangle_calculator(line_1 = int ,line_2 = int,line_3 = int):
-    line_1 = abs(line_1)
-    line_2 = abs(line_2)
-    line_3 = abs(line_3)
+def how_many_lines_equal(line_1 = int,line_2 = int,line_3 = int):
+    if (line_1 == line_2 == line_3):
+        return 3
+    elif ((line_1 == line_2 != line_3) or (line_1 != line_2 == line_3)):
+        return 2
+    else:
+        return 0
+
+def how_many_lines_equal_rectangle(line_1 = int ,line_2 = int,line_3 = int,line_4 = int):
+    if (line_1 == line_2 == line_3 == line_4):
+        return 4
+    elif(line_1 == line_3 != line_2 == line_4):
+        return 2
+    else:
+        return 0
+
+print("""
+_________________________________
+                                |
+Press 1 for calculate triangle. |
+Press 2 for calculate rectangle.|
+________________________________|
+""")
+
+selected_shape = int(input("Please enter a number for calculate the shape: "))
+
+if (selected_shape == 1):
+    print("Please enter lines from small to large!")
+    first_line = abs(int(input("Please enter the first line: ")))
+    second_line = abs(int(input("Please enter the second line: ")))
+    third_line = abs(int(input("Please enter the third line: ")))
+    equal_lines = how_many_lines_equal(first_line,second_line,third_line)
     print("Your triangle is calculating..")
     time.sleep(0.5)
-    if (line_1 == line_2 == line_3):
-        return "Your triangle is Equilateral Triangle."
-    elif (line_1 + line_2 < line_3):
-        return "The shape is not a triangle."
-    elif ((line_1 == line_2 != line_3) or (line_2 == line_3 != line_1) or (line_1 == line_3 != line_2)):
-        return "Your triangle is Isosceles Triangle."
+    if (equal_lines == 3):
+        print("Your triangle is Equilateral Triangle.")
+    elif(first_line + second_line < third_line):
+        print("The shape is not a triangle.")
+    elif (equal_lines == 2):
+        print("Your triangle is Isosceles Triangle.")
     else:
-        return "Your triangle is Normal Triangle :)."
-
-def rectangle_calculator(line_1 = int ,line_2 = int,line_3 = int,line_4 = int):
-    line_1 = abs(line_1)
-    line_2 = abs(line_2)
-    line_3 = abs(line_3)
-    line_4 = abs(line_4)
+        print("Your triangle is Normal Triange :].")
+elif (selected_shape == 2):
+    first_line = abs(int(input("Please enter the first line: ")))
+    second_line = abs(int(input("Please enter the second line: ")))
+    third_line = abs(int(input("Please enter the third line: ")))
+    fourth_line = abs(int(input("Please enter the fourth line: ")))
+    equal_lines = how_many_lines_equal_rectangle(first_line,second_line,third_line,fourth_line)
     print("Your rectangle is calculating..")
     time.sleep(0.5)
-    if (line_1 == line_2 == line_3 == line_4):
-        return "Your rectangle is Square."
-    elif(line_1 == line_3 != line_2 == line_4):
-        return "Your shape is Rectangle."
+    if (equal_lines == 4):
+        print("Your rectangle is Square.")
+    elif (equal_lines == 2):
+        print("Your shape is Rectangle.")
     else:
-        return "Your shape is normal rectangle."
+        print("Your shape is normal rectangle.")
+else:
+    print("Shape is not found.")
 
-print(rectangle_calculator(3,3,3,3))
-    
+
 
 ### Unfortunately we can't calculate many geometrical shape. Because to calculate that, we have to learn their angle. 
 ### We can do this future projects.
