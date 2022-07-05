c830d50b92f957f8df121f5234c124124d900427
h4dicicek <h4dicicek@gmail.com>
2022-07-05 16:19:25 +0300

    end of file

--- embedded_functions/merge_name_surname.py
+++ embedded_functions/merge_name_surname.py
@@ -3,3 +3,6 @@ names = ["Jake","Oliver","Sasha","Melissa","Bethany","Ava"]
 
 #create surname list
 surnames = ["Joses","Davies","O'Ryan","Williams","Miller","Brown"]
+
+for i,j in zip(names,surnames):
+    print(i,j)
