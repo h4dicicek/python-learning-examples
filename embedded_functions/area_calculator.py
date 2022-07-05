b341ad0f95d83a2b77e0648650677693b66fcd22
h4dicicek <h4dicicek@gmail.com>
2022-07-05 16:06:28 +0300

    end of file

--- embedded_functions/area_calculator.py
+++ embedded_functions/area_calculator.py
@@ -3,3 +3,9 @@
 # define function
 def area_calculator(tuple):
     return tuple[0] * tuple[1]
+
+# create list of lines
+list_of_lines = [(3,4),(10,3),(5,6),(1,9)]
+
+# and print result with map function
+print(list(map(area_calculator,list_of_lines)))
