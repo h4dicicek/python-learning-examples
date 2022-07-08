e0980feccb882a67712b3cfec4265d588f956c2c
h4dicicek <h4dicicek@gmail.com>
2022-07-08 13:04:43 +0300

    operations completed

--- sqlite_database/library_operations.py
+++ sqlite_database/library_operations.py
@@ -16,3 +16,37 @@ Press q for quit.
 """)
 
 library = Library()
+
+while True:
+    operation = input("Please enter the operation: ")
+    if (operation == "q"):
+        print("See you soon!")
+        break
+    elif (operation == "1"):
+        print(library.showBooks())
+    elif (operation == "2"):
+        name = input("Which book do you want to query?: ")
+        time.sleep(0.5)
+        print("Wait..")
+        time.sleep(0.7)
+        print(library.queryBookWithName(name))
+    elif (operation == "3"):
+        name = input("Name: ")
+        author = input("Author: ")
+        publisher = input("Publisher: ")
+        type = input("Type: ")
+        new_book = Book(name,author,publisher,type)
+        print("Book is adding..")
+        time.sleep(0.5)
+        library.addBook(new_book)
+        time.sleep(0.7)
+        print("Operation Completed!")
+    elif (operation == "4"):
+        name = input("Which book do you want to delete?: ")
+        print("Book is deleting.")
+        time.sleep(0.5)
+        print(library.deleteBook(name))
+        time.sleep(0.7)
+        print("Operation Completed!")
+    else:
+        print("Operation not found!")
