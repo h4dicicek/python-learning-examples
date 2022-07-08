93f175e4d68cb4b17e9c5273e81c47ebd12e3686
h4dicicek <h4dicicek@gmail.com>
2022-07-08 12:33:12 +0300

    create_connect and disconnect functions defined

--- sqlite_database/library.py
+++ sqlite_database/library.py
@@ -10,3 +10,17 @@ class Book():
 
     def __str__(self):
         return "Name: {}\nAuthor: {}\nPublisher: {}\nType: {}\n".format(self.name,self.author,self.publisher,self.type)
+
+class Library():
+    def __init__(self):
+        self.create_connect()
+    
+    def create_connect(self):
+        self.conn = sqlite3.connect("library.db")
+        self.cursor = self.conn.cursor()
+        query = "CREATE TABLE IF NOT EXISTS books(name TEXT,author TEXT,publisher TEXT,type TEXT)"
+        self.cursor.execute(query)
+        self.conn.commit()
+    
+    def disconnect(self):
+        self.conn.close()
