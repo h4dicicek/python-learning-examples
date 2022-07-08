888d75149a8482d8817d19e85711cf0e6f5f0479
h4dicicek <h4dicicek@gmail.com>
2022-07-08 12:48:59 +0300

    operations table added on file

---
+++ sqlite_database/library_operations.py
@@ -0,0 +1,18 @@
+from dbm.ndbm import library
+from library import *
+print("""
+____________________________
+Operations:
+
+Press 1 for see books.
+
+Press 2 for query books with name.
+
+Press 3 for add book.
+
+Press 4 for delete book.
+
+Press q for quit.
+""")
+
+library = Library()
