74fb6a0f5a491663daad8455094efda6db1d024f
h4dicicek <h4dicicek@gmail.com>
2022-07-09 08:40:49 +0300

    extraPerfectNumber function defined

--- advanced_futures_of_functions_and_decorators/prime_number.py
+++ advanced_futures_of_functions_and_decorators/prime_number.py
@@ -1,3 +1,20 @@
+def extraPerfectNumber(func):
+    def wrapper():
+        print("Perfect number between 1 to 1000")
+        for number in range(1,1001):
+            total = 0
+            i = 1
+            while (i < number):
+                if(number % i == 0):
+                    total += i
+                i += 1
+            if (total == number):
+                print(number)
+        func()
+    return wrapper
+
+
+@extraPerfectNumber
 def prime_number():
     print("Prime number between 1 to 1000")
     for number in range(2,1001):
