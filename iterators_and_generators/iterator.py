8ef4b48edfe1361385dc0d0b7ac8fbb9d1f954b0
h4dicicek <h4dicicek@gmail.com>
2022-07-09 15:17:42 +0300

    iter and next funcs defined. end of class

--- iterators_and_generators/iterator.py
+++ iterators_and_generators/iterator.py
@@ -2,3 +2,13 @@ class force3():
     def __init__(self,max = 0):
         self.max = max
         self.force = 0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if(self.force <= self.max):
+            result = 3 ** self.force
+            self.force += 1
+            return result
+        else:
+            self.force = 0
+            raise StopIteration
