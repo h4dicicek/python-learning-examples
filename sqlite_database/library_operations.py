c1839cd265e8bc1d9cd92fb291dc971abf14aea0
h4dicicek <h4dicicek@gmail.com>
2022-07-17 14:31:13 +0300

    Revert "Merge branch 'sqlite_database_'"
    This reverts commit 5176b5d7ba2e417a0f00fbce6bf9c0292ab1dc76, reversing
    changes made to a85e859c4032a6a32b14c50820bfee49cc58ea1f.

--- sqlite_database/library_operations.py
+++ sqlite_database/library_operations.py
@@ -1,60 +1,52 @@
 from dbm.ndbm import library
 from library import *
-
-print(
-    """
+print("""
 ____________________________
 Operations:
 
 Press 1 for see books.
 
-Press 2 for get book by name.
+Press 2 for query books with name.
 
-Press 3 for get books by name which books are have same name.
+Press 3 for add book.
 
-Press 4 for add book.
-
-Press 5 for delete book.
+Press 4 for delete book.
 
 Press q for quit.
-"""
-)
+""")
 
 library = Library()
 
 while True:
     operation = input("Please enter the operation: ")
-
-    if operation == "q":
+    if (operation == "q"):
         print("See you soon!")
         break
-
-    elif operation == "1":
-        library.showBooks()
-
-    elif operation == "2":
+    elif (operation == "1"):
+        print(library.showBooks())
+    elif (operation == "2"):
         name = input("Which book do you want to query?: ")
-        print(library.getBookByName(name))
-
-    elif operation == "3":
-        name = input("Which book do you want to query?: ")
-        library.getBooksByName(name)
-
-    elif operation == "4":
+        time.sleep(0.5)
+        print("Wait..")
+        time.sleep(0.7)
+        print(library.queryBookWithName(name))
+    elif (operation == "3"):
         name = input("Name: ")
         author = input("Author: ")
         publisher = input("Publisher: ")
         type = input("Type: ")
-        new_book = Book(name, author, publisher, type)
+        new_book = Book(name,author,publisher,type)
         print("Book is adding..")
+        time.sleep(0.5)
         library.addBook(new_book)
+        time.sleep(0.7)
         print("Operation Completed!")
-
-    elif operation == "5":
+    elif (operation == "4"):
         name = input("Which book do you want to delete?: ")
         print("Book is deleting.")
-        library.deleteBook(name)
+        time.sleep(0.5)
+        print(library.deleteBook(name))
+        time.sleep(0.7)
         print("Operation Completed!")
-
     else:
         print("Operation not found!")
